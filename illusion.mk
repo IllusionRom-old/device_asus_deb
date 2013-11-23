69e12ddceef286e8985809e43b71a902fd3cae94
aznrice <aznrice2k4@gmail.com>
2013-11-22 22:27:02 -0600

    Fix up fingerprint
    Change-Id: I9d63cc4a450a4c62c503cbc2425c7d2549bc9ac1

--- illusion.mk
+++ illusion.mk
@@ -3,6 +3,8 @@
 TARGET_SCREEN_HEIGHT := 1920
 TARGET_SCREEN_WIDTH := 1200
 
+PRODUCT_RELEASE_NAME := deb
+
 $(call inherit-product, device/asus/deb/full_deb.mk)
 
 $(call inherit-product, vendor/illusion/config/common.mk)
@@ -16,7 +18,7 @@ PRODUCT_BRAND := google
 PRODUCT_MODEL := Nexus 7
 PRODUCT_MANUFACTURER := asus
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=razorg BUILD_FINGERPRINT=google/razorg/deb:4.4/KRT16O/907817:user/release-keys PRIVATE_BUILD_DESC="razorg-user 4.4 KRT16O 907817 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=razorg BUILD_FINGERPRINT="google/razorg/deb:4.4/KRT16O/907817:user/release-keys" PRIVATE_BUILD_DESC="razorg-user 4.4 KRT16O 907817 release-keys"
 
 # bootanimation
 PRODUCT_COPY_FILES += \
